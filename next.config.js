63365c1908f1dc97ef0b22328a508815931b0543
Choazmmm <azamishere7@gmail.com>
2025-11-28 11:40:40 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // JANGAN tambahkan appDir jika pakai Pages Router
-};
-
-module.exports = nextConfig;
